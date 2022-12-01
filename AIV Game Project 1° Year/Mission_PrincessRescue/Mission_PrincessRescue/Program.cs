fbb7b0877d04f414e083cab716df4b2a61508c84
Giuseppe Acanfora <101001028+AcanforaGiuseppe@users.noreply.github.com>
2022-12-01 09:44:04 +0100

    Add files via upload

---
+++ AIV Game Project 1° Year/Mission_PrincessRescue/Mission_PrincessRescue/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Mission_PrincessRescue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Game.Init();
+
+            Game.Play();
+
+        }
+    }
+}

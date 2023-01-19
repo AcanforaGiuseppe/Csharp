c510cba43a09d1fe2b65057945f45c6596f3132a
Giuseppe Acanfora <101001028+AcanforaGiuseppe@users.noreply.github.com>
2023-01-19 14:12:12 +0100

    Add files via upload

---
+++ DotNet Project/BulkyBook.Models/CoverType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class CoverType
+    {
+        // Attribute that stays for PK (Primary Key)
+        [Key]
+        public int Id { get; set; }
+
+        // Displays a name that is not the one of the variable
+        [Display(Name = "Cover Type")]
+        [Required]
+        // Gives to the field a Max Length of the input
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
---
+++ DotNet Project/BulkyBook.Models/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; }
+        // Range stays for a given input between 2 values
+        [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+        public int Count { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        // Indicates an attribute that will not be shown on the mapping present in the db
+        [NotMapped]
+        public double Price { get; set; }
+    }
+}
